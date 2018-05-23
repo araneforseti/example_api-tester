d61595692c112621888130cbd4c83717dd7a8422
Sarah <sarahforseti@gmail.com>
2018-05-23 14:19:21 -0400

    Updating api-tester

--- api_spec/contract_spec.rb
+++ api_spec/contract_spec.rb
@@ -1,19 +1,20 @@
 require_relative 'api_spec_helper.rb'
 require 'rack/test'
-require 'tester'
-require 'tester/definition/endpoint'
-require 'tester/definition/api_contract'
-require 'tester/definition/request'
-require 'tester/definition/response'
-require 'tester/definition/fields/field'
-require 'tester/definition/fields/object_field'
-require 'tester/definition/fields/array_field'
-require 'tester/definition/fields/number_field'
-require 'tester/api_tester'
-require 'tester/modules/format'
-require 'tester/modules/good_case'
-require 'tester/modules/typo'
-require 'tester/modules/unused_fields'
+require 'api-tester'
+require 'api-tester/test_helper'
+require 'api-tester/definition/endpoint'
+require 'api-tester/config'
+require 'api-tester/definition/api_contract'
+require 'api-tester/definition/request'
+require 'api-tester/definition/response'
+require 'api-tester/definition/fields/field'
+require 'api-tester/definition/fields/object_field'
+require 'api-tester/definition/fields/array_field'
+require 'api-tester/definition/fields/number_field'
+require 'api-tester/modules/format'
+require 'api-tester/modules/good_case'
+require 'api-tester/modules/typo'
+require 'api-tester/modules/unused_fields'
 
 include Rack::Test::Methods
 
@@ -21,48 +22,52 @@ describe 'Contract' do
   let(:base_url) { "http://localhost:4567/api/v1" }
 
   it 'match contract' do
-    contract = ApiContract.new "Janky API"
+    contract = ApiTester::ApiContract.new "Janky API"
 
-    sheets_endpoint = Endpoint.new "Sheets", "#{base_url}/sheets"
-    post_request = Request.new.add_field(Field.new "id")
-        .add_field(Field.new "name")
-        .add_field(NumberField.new "strength")
-        .add_field(NumberField.new "dexterity")
-        .add_field(NumberField.new "constitution")
-        .add_field(NumberField.new "will")
-        .add_field(NumberField.new "intelligence")
-        .add_field(NumberField.new "charisma")
-    expected_response = Response.new(200)
-        .add_field(Field.new "name")
-        .add_field(NumberField.new "strength")
-        .add_field(NumberField.new "dexterity")
-        .add_field(NumberField.new "constitution")
-        .add_field(NumberField.new "will")
-        .add_field(NumberField.new "intelligence")
-        .add_field(NumberField.new "charisma")
+    sheets_endpoint = ApiTester::Endpoint.new "Sheets", "#{base_url}/sheets"
+    post_request = ApiTester::Request.new.add_field(ApiTester::Field.new "id")
+        .add_field(ApiTester::Field.new "name")
+        .add_field(ApiTester::NumberField.new "strength")
+        .add_field(ApiTester::NumberField.new "dexterity")
+        .add_field(ApiTester::NumberField.new "constitution")
+        .add_field(ApiTester::NumberField.new "will")
+        .add_field(ApiTester::NumberField.new "intelligence")
+        .add_field(ApiTester::NumberField.new "charisma")
+    expected_response = ApiTester::Response.new(200)
+        .add_field(ApiTester::Field.new "name")
+        .add_field(ApiTester::NumberField.new "strength")
+        .add_field(ApiTester::NumberField.new "dexterity")
+        .add_field(ApiTester::NumberField.new "constitution")
+        .add_field(ApiTester::NumberField.new "will")
+        .add_field(ApiTester::NumberField.new "intelligence")
+        .add_field(ApiTester::NumberField.new "charisma")
     sheets_endpoint.add_method :get, expected_response
     sheets_endpoint.add_method :post, expected_response, post_request
     sheets_endpoint.test_helper = SheetCreator.new base_url
     contract.add_endpoint sheets_endpoint
 
-    sheet_endpoint = Endpoint.new "Sheets", "#{base_url}/sheets/{testSheet}"
+    sheet_endpoint = ApiTester::Endpoint.new "Sheets", "#{base_url}/sheets/{testSheet}"
     sheet_endpoint.add_path_param "testSheet"
     sheet_endpoint.test_helper = SheetCreator.new base_url
-    sheet_field = ObjectField.new("sheet").with_field(Field.new "id", "testSheet")
-        .with_field(Field.new "name")
-        .with_field(NumberField.new "strength")
-        .with_field(NumberField.new "dexterity")
-        .with_field(NumberField.new "constitution")
-        .with_field(NumberField.new "will")
-        .with_field(NumberField.new "intelligence")
-        .with_field(NumberField.new "charisma")
-        .with_field(ArrayField.new "skills")
-    expected_response = Response.new(200).add_field(sheet_field)
-    sheet_endpoint.add_method SupportedVerbs::GET, expected_response
-    sheet_endpoint.add_method SupportedVerbs::POST, expected_response, post_request
+    sheet_field = ApiTester::ObjectField.new("sheet").with_field(ApiTester::Field.new "id", "testSheet")
+        .with_field(ApiTester::Field.new "name")
+        .with_field(ApiTester::NumberField.new "strength")
+        .with_field(ApiTester::NumberField.new "dexterity")
+        .with_field(ApiTester::NumberField.new "constitution")
+        .with_field(ApiTester::NumberField.new "will")
+        .with_field(ApiTester::NumberField.new "intelligence")
+        .with_field(ApiTester::NumberField.new "charisma")
+        .with_field(ApiTester::ArrayField.new "skills")
+    expected_response = ApiTester::Response.new(200).add_field(sheet_field)
+    sheet_endpoint.add_method ApiTester::SupportedVerbs::GET, expected_response
+    sheet_endpoint.add_method ApiTester::SupportedVerbs::POST, expected_response, post_request
 
-    tester = ApiTester.new(contract).with_module(Format.new).with_module(GoodCase.new).with_module(Typo.new).with_module(UnusedFields.new)
-    expect(tester.go).to be false
+    config = ApiTester::Config.new
+      .with_module(ApiTester::Format.new)
+      .with_module(ApiTester::GoodCase.new)
+      .with_module(ApiTester::Typo.new)
+      .with_module(ApiTester::UnusedFields.new)
+    expect(ApiTester.go(contract, config)).to be false
   end
 end
 
